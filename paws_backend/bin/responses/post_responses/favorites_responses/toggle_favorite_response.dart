3ea362ec64975b583d86a640d56e0a354394c6ef
AhmadAlmudhi <a.almudhi1419@gmail.com>
2023-05-26 19:12:59 +0300

    toggle favorite

--- paws_backend/bin/responses/post_responses/favorites_responses/toggle_favorite_response.dart
+++ paws_backend/bin/responses/post_responses/favorites_responses/toggle_favorite_response.dart
@@ -1,11 +1,10 @@
-import 'dart:convert';
 import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:shelf/shelf.dart';
 import '../../../response_messages/bad_request.dart';
 import '../../../response_messages/success.dart';
 import '../../../services/supabase/supabase_env.dart';
 
-Future<Response> addToFavoritesHandler(Request req, String postId) async {
+Future<Response> toggleFavoriteHandler(Request req, String postId) async {
   try {
     final jwt = JWT.decode(req.headers["authorization"]!);
     final authId = jwt.payload["sub"];
@@ -14,22 +13,29 @@ Future<Response> addToFavoritesHandler(Request req, String postId) async {
     final fromUsers = supabase.from("users");
     final fromPosts = supabase.from("posts");
 
-    final List userFavorites = json.decode((await fromUsers
+    final List userFavorites = ((await fromUsers
         .select("favorites")
         .eq("auth_id", authId))[0]["favorites"]);
-
-    userFavorites.add(postId);
-
-    await fromUsers.update({"favorites": json.encode(userFavorites)}).eq(
-      "auth_id",
-      authId,
-    );
 
     int favoritesNumber = (await fromPosts
         .select("favorites_number")
         .eq("post_id", int.parse(postId)))[0]["favorites_number"];
 
-    ++favoritesNumber;
+    String msg = "post has been removed from favorites!";
+
+    if (userFavorites.contains(int.parse(postId))) {
+      userFavorites.remove(int.parse(postId));
+      --favoritesNumber;
+    } else {
+      userFavorites.add(int.parse(postId));
+      ++favoritesNumber;
+      msg = "post has been added to favorites!";
+    }
+
+    await fromUsers.update({"favorites": userFavorites}).eq(
+      "auth_id",
+      authId,
+    );
 
     await fromPosts.update({"favorites_number": favoritesNumber}).eq(
       "post_id",
@@ -37,12 +43,10 @@ Future<Response> addToFavoritesHandler(Request req, String postId) async {
     );
 
     return Success().responseMessage(
-      message: "post has been added to favorites!",
+      message: msg,
       data: {"favorites": userFavorites},
     );
   } catch (e) {
-    print(e);
-
     return BadRequest().responseMessage(message: "Something went wrong!");
   }
 }
--- paws_backend/bin/routers/post_routers/favorites_router.dart
+++ paws_backend/bin/routers/post_routers/favorites_router.dart
@@ -1,13 +1,11 @@
 import 'package:shelf_router/shelf_router.dart';
-import '../../responses/post_responses/favorites_responses/add_to_favorites_response.dart';
 import '../../responses/post_responses/favorites_responses/get_favorites_number_response.dart';
-import '../../responses/post_responses/favorites_responses/remove_from_favorites_response.dart';
+import '../../responses/post_responses/favorites_responses/toggle_favorite_response.dart';
 
 class FavoritesRouter {
   Router get handler {
     final router = Router()
-      ..post("/add/<post_id>", addToFavoritesHandler)
-      ..delete("/remove/<post_id>", removeFromFavoritesHandler)
+      ..post("/toggle/<post_id>", toggleFavoriteHandler)
       ..get('/get_number/<post_id>', getFavoritesNumberHandler);
 
     return router;
