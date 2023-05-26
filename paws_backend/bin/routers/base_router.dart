5aa8e79e03ea1f60591e30c86cb4a77ee4c8212c
AhmadAlmudhi <a.almudhi1419@gmail.com>
2023-05-26 16:59:49 +0300

    add post router to base router

--- paws_backend/bin/routers/base_router.dart
+++ paws_backend/bin/routers/base_router.dart
@@ -2,6 +2,7 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 import '../responses/base_responses/root_response.dart';
 import 'auth_router.dart';
+import 'post_routers/post_router.dart';
 import 'user_router.dart';
 
 class BaseRouter {
@@ -10,6 +11,7 @@ class BaseRouter {
       ..get('/', rootHandler)
       ..mount("/auth", AuthRouter().handler)
       ..mount("/user", UserRouter().handler)
+      ..mount("/post", PostRouter().handler)
       ..all('/<name|.*>', (Request _) {
         return Response.notFound("Route not found please change your route!");
       });
