abf94092bd8288ac3507ccf8c521005da5a694d5
Renad <Renadxx17@gmail.com>
2023-05-27 23:57:22 +0300

    new endpoint: add and update image profile

---
+++ paws_backend/bin/responses/user_responses/add_profile_image.dart
@@ -0,0 +1,96 @@
+
+
+import 'dart:io';
+
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
+import 'package:shelf/shelf.dart';
+
+import '../../response_messages/bad_request.dart';
+import '../../response_messages/success.dart';
+import '../../services/supabase/supabase_env.dart';
+
+addProfileImage(Request req) async {
+  try{
+  final byte = await req.read().expand((element) => element).toList();
+  final userInfo = JWT.decode(req.headers["authorization"]!);
+  final image = await createImage(byte: byte);
+  final hasImage = await checkImageProfile(idAuth: userInfo.payload["sub"]);
+  final url =
+      await uploadImage(file: image, found: hasImage, id: userInfo.payload["sub"]);
+  await image.delete();
+  await sendURL(url: url,idAuth:userInfo.payload["sub"]);
+
+  return Success().responseMessage(message: "upload done");
+}catch(error){
+  return BadRequest().responseMessage(message: "something went wrong");
+}
+}
+//create image inside project directory then return file
+Future<File> createImage({required List<int> byte}) async {
+  final file = File("bin/image/test.png");
+  await file.writeAsBytes(byte);
+
+  return file;
+}
+
+//check if user has image in supabase storage then return boolean
+
+checkImageProfile({required String idAuth}) async {
+  final listImage = await SupabaseEnv()
+      .supabase
+      .storage
+      .from("imageProfile")
+      .list(path: "images");
+  for (var element in listImage) {
+    print(element.name);
+    if (element.name.contains(idAuth)) {
+      print("-----checkImageProfile---");
+
+      return true;
+    }
+  }
+  print("-----checkImageProfile---");
+  return false;
+}
+
+//create function for upload image to supabase then return url
+
+uploadImage({
+  required bool found,
+  required File file,
+  required String id,
+}) async {
+  final supabase = SupabaseEnv().supabase.storage.from("imageProfile");
+  if (found) {
+    await supabase.update('images/$id.png', file);
+  } else {
+    await supabase.upload('images/$id.png', file);
+  }
+  final url = await supabase.getPublicUrl('images/$id.png');
+  print("-----uploadImage---");
+
+  return url;
+}
+
+//send url to table image in database
+
+sendURL({ required String url,required String idAuth}) async {
+  final supabase = SupabaseEnv().supabase.from("users");
+
+  final result = await supabase.update({"image": url}).eq("auth_id", idAuth);
+  print("-----sendURL---");
+
+  return result;
+}
+
+//for delete image fromn server
+
+deleteImageProfile({required String imageName}) async {
+  final listImage = await SupabaseEnv()
+      .supabase
+      .storage
+      .from("imageProfile")
+      .remove(["images/$imageName"]);
+
+  return false;
+}
--- paws_backend/bin/routers/user_router.dart
+++ paws_backend/bin/routers/user_router.dart
@@ -1,6 +1,7 @@
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 import '../middlewares/check_token_middleware.dart';
+import '../responses/user_responses/add_profile_image.dart';
 import '../responses/user_responses/read_profile_response.dart';
 import '../responses/user_responses/read_favorites_response.dart';
 import '../responses/user_responses/update_profile_response.dart';
@@ -10,7 +11,8 @@ class UserRouter {
     final router = Router()
       ..get("/read_profile", readProfileHandler)
       ..put("/update_profile", updateProfileHandler)
-      ..get("/read_favorites", readFavoritesHandler);
+      ..get("/read_favorites", readFavoritesHandler)
+      ..post("/d", addProfileImage);
 
     final pipline =
         Pipeline().addMiddleware(checkTokenMiddleware()).addHandler(router);
