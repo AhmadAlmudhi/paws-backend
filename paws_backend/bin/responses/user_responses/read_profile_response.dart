8af88168ccd58ac2f2566356330283b9802de995
AhmadAlmudhi <a.almudhi1419@gmail.com>
2023-05-27 16:23:29 +0300

    edit profile and updated results

--- paws_backend/bin/responses/user_responses/read_profile_response.dart
+++ paws_backend/bin/responses/user_responses/read_profile_response.dart
@@ -5,38 +5,38 @@ import '../../response_messages/bad_request.dart';
 import '../../response_messages/success.dart';
 import '../../services/supabase/supabase_env.dart';
 
-readProfileHandler(Request req) async{
-  try{
-  final header = req.headers;
-  final supabase = SupabaseEnv().supabase;
-  final selectUser = supabase.from("users");
-  final jwt = JWT.decode(header['authorization']!);
-  final userAuth = jwt.payload["sub"];
+readProfileHandler(Request req) async {
+  try {
+    final header = req.headers;
+    final supabase = SupabaseEnv().supabase;
+    final selectUser = supabase.from("users");
+    final jwt = JWT.decode(header['authorization']!);
+    final userAuth = jwt.payload["sub"];
 
-  final id = await selectUser.select("user_id").eq("auth_id", userAuth);
+    final id = await selectUser.select("user_id").eq("auth_id", userAuth);
 
-  final user = (await supabase
-      .from("users")
-      .select()
-      .eq("auth_id", userAuth))[0];
+    final user =
+        (await supabase.from("users").select().eq("auth_id", userAuth))[0];
 
-  final details = (await supabase
-      .from("details")
-      .select()
-      .eq("user_id", id[0]["user_id"]))[0];
+    final details = (await supabase
+        .from("details")
+        .select()
+        .eq("user_id", id[0]["user_id"]))[0];
 
-  final contact = (await supabase
-      .from("contacts")
-      .select()
-      .eq("user_id", id[0]["user_id"]))[0];
+    final contact = (await supabase
+        .from("contacts")
+        .select()
+        .eq("user_id", id[0]["user_id"]))[0];
 
-  Map userMap = {
-      ...user,...details,
+    Map userMap = {
+      "info": user,
+      "details": details,
       "contact": contact,
     };
 
-      return Success().responseMessage(message: 'here is your profile', data: userMap);
-  }catch(error){
+    return Success()
+        .responseMessage(message: 'here is your profile', data: userMap);
+  } catch (error) {
     print(error);
 
     return BadRequest().responseMessage(message: "something went wrong");
--- paws_backend/bin/responses/user_responses/update_profile_response.dart
+++ paws_backend/bin/responses/user_responses/update_profile_response.dart
@@ -8,8 +8,8 @@ import '../../response_messages/bad_request.dart';
 import '../../response_messages/success.dart';
 import '../../services/supabase/supabase_env.dart';
 
-updateProfileHandler(Request req) async{
-   try {
+updateProfileHandler(Request req) async {
+  try {
     final Map body = json.decode(await req.readAsString());
     final checkEmail = body.keys.contains("email");
     final supabase = SupabaseEnv().supabase;
@@ -17,8 +17,8 @@ updateProfileHandler(Request req) async{
     final userAuth = jwt.payload["sub"];
     final fromProfile = supabase.from("users");
     final fromDetails = supabase.from("details");
-    final fromContact =supabase.from("contacts");
-    
+    final fromContact = supabase.from("contacts");
+
     if (checkEmail) {
       await supabase.auth.admin.updateUserById(
         jwt.payload["sub"],
@@ -30,51 +30,58 @@ updateProfileHandler(Request req) async{
         .select("email, name, bio")
         .eq("auth_id", jwt.payload["sub"]);
 
-   final id = await fromProfile.select("user_id").eq("auth_id", userAuth);
-        
-   final details = await fromDetails.select("phone, country, city,gender,age")
+    final id = await fromProfile.select("user_id").eq("auth_id", userAuth);
+
+    final details = await fromDetails
+        .select("phone, country, city,gender,age")
         .eq("user_id", id[0]["user_id"]);
 
-   final contacts = await fromContact .select("phone, whatsapp, email")
+    final contacts = await fromContact
+        .select("phone, whatsapp, email")
         .eq("user_id", id[0]["user_id"]);
 
-   final updatedUser = await fromProfile
+    final updatedUser = await fromProfile
         .update({
           "name": body["name"] ?? user[0]["name"],
           "email": body["email"] ?? user[0]["email"],
-          "bio": body["bio"] ?? user[0]["bio"],})
+          "bio": body["bio"] ?? user[0]["bio"],
+        })
         .eq("auth_id", jwt.payload["sub"])
         .select("username, email, name, bio");
 
-   final updatedDetails = await fromDetails
+    final updatedDetails = await fromDetails
         .update({
           "phone": body["phone"] ?? details[0]["phone"],
           "country": body["country"] ?? details[0]["country"],
           "city": body["city"] ?? details[0]["city"],
           "gender": body["gender"] ?? details[0]["gender"],
-          "age": body["age"] ?? details[0]["age"],})
+          "age": body["age"] ?? details[0]["age"],
+        })
         .eq("user_id", id[0]["user_id"])
         .select("phone, country, city,gender,age");
 
-   final updatedContact = await fromContact
+    final updatedContact = await fromContact
         .update({
           "phone": body["phone"] ?? contacts[0]["phone"],
           "whatsapp": body["whatsapp"] ?? contacts[0]["whatsapp"],
-          "email": body["email"] ?? contacts[0]["email"],})
+          "email": body["email"] ?? contacts[0]["email"],
+        })
         .eq("user_id", id[0]["user_id"])
         .select("phone, whatsapp, email");
 
-   Map userMap = {
-      ...updatedUser[0],...updatedDetails[0],
+    Map userMap = {
+      "info": updatedUser[0],
+      "details": updatedDetails[0],
       "contact": updatedContact,
     };
 
     return Success().responseMessage(
-      message: "profile has been updated",data: userMap,);
-  }on RangeError{
+      message: "profile has been updated",
+      data: userMap,
+    );
+  } on RangeError {
     return BadRequest().responseMessage(message: "invalid input");
-  }
-   catch (error) {
+  } catch (error) {
     print(error);
 
     return BadRequest().responseMessage(message: "something went wrong");
