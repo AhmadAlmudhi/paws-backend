858749904ffabc8afc1a251b9694f5e07fd751a4
AhmadAlmudhi <a.almudhi1419@gmail.com>
2023-05-27 20:26:05 +0300

    read post endpoints

--- paws_backend/bin/responses/post_responses/read_all_posts_response.dart
+++ paws_backend/bin/responses/post_responses/read_all_posts_response.dart
@@ -1,5 +1,42 @@
 import 'package:shelf/shelf.dart';
+import '../../response_messages/not_found.dart';
+import '../../response_messages/success.dart';
+import '../../services/supabase/supabase_env.dart';
 
-Response readAllPostsHandler(Request _) {
-  return Response.ok('read all posts is working!');
+Future<Response> readAllPostsHandler(Request _) async {
+  try {
+    final List<Map> result = [];
+
+    final supabase = SupabaseEnv().supabase;
+
+    final posts = await supabase.from("posts").select();
+
+    for (var element in posts) {
+      final Map userInfo = (await supabase
+          .from("users")
+          .select("name, username, image")
+          .eq("user_id", element["user_id"]))[0];
+
+      final Map animalInfo = (await supabase
+          .from("animals")
+          .select("type, breed")
+          .eq("post_id", element["post_id"]))[0];
+
+      final Map postInfo = element;
+
+      Map post = {};
+      post.addAll(userInfo);
+      post.addAll(postInfo);
+      post.addAll(animalInfo);
+
+      result.add(post);
+    }
+
+    return Success().responseMessage(
+      message: "success",
+      data: {"posts:": result},
+    );
+  } catch (error) {
+    return NotFound().responseMessage(message: "posts not found");
+  }
 }
--- paws_backend/bin/responses/post_responses/read_post_response.dart
+++ paws_backend/bin/responses/post_responses/read_post_response.dart
@@ -1,5 +1,32 @@
 import 'package:shelf/shelf.dart';
+import '../../response_messages/not_found.dart';
+import '../../response_messages/success.dart';
+import '../../services/supabase/supabase_env.dart';
 
-Response readPostHandler(Request _) {
-  return Response.ok('read post is working!');
+Future<Response> readPostHandler(Request _, String postId) async {
+  try {
+    final supabase = SupabaseEnv().supabase;
+
+    final postValues =
+        (await supabase.from("posts").select().eq("post_id", postId))[0];
+
+    final Map userInfo = (await supabase
+        .from("users")
+        .select("name, username, image")
+        .eq("user_id", postValues["user_id"]))[0];
+
+    final Map animalInfo =
+        (await supabase.from("animals").select().eq("post_id", postId))[0];
+
+    return Success().responseMessage(
+      message: "success",
+      data: {
+        "user_info:": [userInfo],
+        "post_info:": [postValues],
+        "animal_info:": [animalInfo],
+      },
+    );
+  } catch (error) {
+    return NotFound().responseMessage(message: "post not found");
+  }
 }
